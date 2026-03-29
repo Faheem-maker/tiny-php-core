51af731a3e941e72eb5ee4ecd7c39d78b38ee214
Faheem Anis <faheem@cartzlink.com>
2026-03-29 11:41:44 +0500

    Added tests for component loading

--- tests/Unit/ApplicationTest.php
+++ tests/Unit/ApplicationTest.php
@@ -1,11 +1,41 @@
 <?php
 
+use framework\components\PathManager;
+use framework\tests\TestApplication;
 use PHPUnit\Framework\TestCase;
 
 class ApplicationTest extends TestCase
 {
-    public function testBasicAssertion()
+    public function testAppCreation()
     {
-        $this->assertTrue(true);
+        $app = createApp();
+
+        $this->assertInstanceOf(TestApplication::class, $app);
+    }
+
+    public function testComponentRegistration() {
+        $app = createApp();
+
+        $app->registerComponent('path', new PathManager());
+
+        $this->assertInstanceOf(PathManager::class, $app->path);
+    }
+
+    public function testLazyComponent() {
+        $app = createApp();
+
+        $app->registerComponent('path', PathManager::class);
+
+        $this->assertInstanceOf(PathManager::class, $app->path);
+    }
+
+    public function testFactoryComponent() {
+        $app = createApp();
+
+        $app->registerComponent('path', function() {
+            return new PathManager();
+        });
+
+        $this->assertInstanceOf(PathManager::class, $app->path);
     }
 }
