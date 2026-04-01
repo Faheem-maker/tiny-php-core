541521bc51755a7c1bc8da79bfb91083112864b0
Faheem Anis <faheem@cartzlink.com>
2026-04-01 20:20:37 +0500

    Added tests for "resolve" method

---
+++ tests/Unit/PathTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use framework\tests\TestApplication;
+use PHPUnit\Framework\TestCase;
+
+class PathTest extends TestCase
+{
+    public function testResolve()
+    {
+        $app = createApp();
+
+        $this->assertEquals(realpath(__DIR__ . '/../'), $app->path->resolve('@root'));
+        $this->assertEquals(realpath(__DIR__ . '/../'), $app->path->resolve('@root/'));
+        $this->assertEquals(realpath(__DIR__ . '/../runtime'), $app->path->resolve('@runtime/'));
+        $this->assertEquals(realpath(__DIR__ . '/../runtime/logs'), $app->path->resolve('@runtime/logs'));
+        $this->assertEquals(realpath(__DIR__ . '/../Unit'), $app->path->resolve('/Unit'));
+        $this->assertEquals(realpath(__DIR__ . '/../Unit'), $app->path->resolve('/Unit/'));
+    }
+}
