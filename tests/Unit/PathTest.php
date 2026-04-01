90fd8f97e01337b2f69015180105d0363a257fb6
Faheem Anis <faheem@cartzlink.com>
2026-04-01 20:37:56 +0500

    Added tests for "resolveWithDefault" and "replaceDots"

--- tests/Unit/PathTest.php
+++ tests/Unit/PathTest.php
@@ -9,12 +9,30 @@ class PathTest extends TestCase
     {
         $app = createApp();
 
+        define('DS', DIRECTORY_SEPARATOR);
+
         $this->assertEquals(realpath(__DIR__ . '/../'), $app->path->resolve('@root'));
         $this->assertEquals(realpath(__DIR__ . '/../'), $app->path->resolve('@root/'));
         $this->assertEquals(realpath(__DIR__ . '/../runtime'), $app->path->resolve('@runtime/'));
         $this->assertEquals(realpath(__DIR__ . '/../runtime/logs'), $app->path->resolve('@runtime/logs'));
         $this->assertEquals(realpath(__DIR__ . '/../Unit'), $app->path->resolve('/Unit'));
         $this->assertEquals(realpath(__DIR__ . '/../Unit'), $app->path->resolve('/Unit/'));
-        $this->assertEquals('runtime/logs/assets', $app->path->resolve('runtime//logs/assets/'));
+        $this->assertEquals('runtime' . DS . 'logs' . DS . 'assets', $app->path->resolve('runtime//logs/assets/'));
+    }
+
+    public function testResolveWithDefault()
+    {
+        $app = createApp();
+
+        $this->assertEquals(realpath(__DIR__ . '/../Unit'), $app->path->resolveWithDefault('Unit', '@root'));
+        $this->assertEquals(realpath(__DIR__ . '/../runtime/logs'), $app->path->resolveWithDefault('@runtime/logs', '@root'));
+    }
+
+    public function testReplaceDots()
+    {
+        $app = createApp();
+
+        $this->assertEquals('views/home/index', $app->path->replaceDots('views.home.index'));
+        $this->assertEquals('views/home/index.html.php', $app->path->replaceDots('views.home.index\.html\.php'));
     }
 }
